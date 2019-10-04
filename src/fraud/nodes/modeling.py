2f2ef5e2bd5abbf49de6710c240bff80437ef7cc
cnielly <cyprien78@gmail.com>
2019-10-04 16:10:36 +0200

    import needed libraries in the modeling python script for undersampling methods

--- src/fraud/nodes/modeling.py
+++ src/fraud/nodes/modeling.py
@@ -1,7 +1,9 @@
 from imblearn.metrics import sensitivity_score, specificity_score, geometric_mean_score 
-from imblearn.under_sampling import RandomUnderSampler, NearMiss, EditedNearestNeighbours, NeighbourhoodCleaningRule
+from imblearn.under_sampling import CondensedNearestNeighbour, EditedNearestNeighbours, RepeatedEditedNearestNeighbourghbours, AllKNN, InstanceHardnessThreshold, NearMiss, NeighbourhoodCleaningRule, RandomUnderSampler, TomekLinks
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
+from sklearn import svm
+import lightgbm as lgb
 
 from src.fraud.nodes.metrics import *
 import pandas as pd
@@ -9,7 +11,7 @@ import pandas as pd
 
 def sampling(X_train, y_train, X_test, y_test, sampling_instances, model_instances, func):
     """
-    Function to test different samlping methids on different models. 
+    Function to test different sampling methods on different models. 
     For each sampling methods in list(sampling_instance) and each model in list(models) compute the set of metrics of metrics function 
 
     :input X_train, y_train, X_test, y_test: np.array or pd.DataFrame of data
@@ -24,14 +26,14 @@ def sampling(X_train, y_train, X_test, y_test, sampling_instances, model_instanc
     # go through all sampling methods
     for sampling_instance in sampling_instances:
         if sampling_instance is not None:
-            print('fitting sampling '+ str(sampling_instances.index(sampling_instance) + 1) + ' on ' +  str(len(sampling_instances)))
+            print('fitting sampling '+ str(sampling_instances.index(sampling_instance) + 1) + ' over ' +  str(len(sampling_instances)))
             X_train1, y_train1 = sampling_instance.fit_resample(X=X_train, y=y_train)
         else:
             X_train1, y_train1 = X_train, y_train
         
         # Go through all models
         for model_instance in model_instances:
-            print('fitting model ' + str(model_instances.index(model_instance) + 1) + ' on ' +  str(len(model_instances)))
+            print('fitting model ' + str(model_instances.index(model_instance) + 1) + ' over ' +  str(len(model_instances)))
             model_instance.fit(X_train1, y_train1)
             metrics.append(func(y_test, model_instance.predict(X_test)))
 
