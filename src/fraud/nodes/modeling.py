7649d68303b4b127079af3c3bc8aeb1394512c89
mdeborne <mathieu.deborne@mail.mcgill.ca>
2019-10-04 18:21:01 +0200

    update modelling.py thresold function

--- src/fraud/nodes/modeling.py
+++ src/fraud/nodes/modeling.py
@@ -1,4 +1,4 @@
-from imblearn.metrics import sensitivity_score, specificity_score, geometric_mean_score 
+from imblearn.metrics import sensitivity_score, specificity_score, geometric_mean_score
 from imblearn.under_sampling import CondensedNearestNeighbour, EditedNearestNeighbours, RepeatedEditedNearestNeighbours, AllKNN, InstanceHardnessThreshold, NearMiss, NeighbourhoodCleaningRule, RandomUnderSampler, TomekLinks
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
@@ -11,17 +11,17 @@ import pandas as pd
 
 def sampling(X_train, y_train, X_test, y_test, sampling_instances, model_instances, func):
     """
-    Function to test different sampling methods on different models. 
-    For each sampling methods in list(sampling_instance) and each model in list(models) compute the set of metrics of metrics function 
+    Function to test different sampling methods on different models.
+    For each sampling methods in list(sampling_instance) and each model in list(models) compute the set of metrics of metrics function
 
     :input X_train, y_train, X_test, y_test: np.array or pd.DataFrame of data
     :input sampling_instance: list of instances of sampling methods (tested for all methods of Imblearn)
     :input model_instances: list of instances of models (from SKLearn or with SKLearn API)
     :input func: function to compute metrics - either compute_metrics or compute_main_metrics
-    
-    :output metrics: pd.Dataframe with metrics as column and model_x_sampling 
+
+    :output metrics: pd.Dataframe with metrics as column and model_x_sampling
     """
-    
+
     metrics = []
     # go through all sampling methods
     for sampling_instance in sampling_instances:
@@ -30,7 +30,7 @@ def sampling(X_train, y_train, X_test, y_test, sampling_instances, model_instanc
             X_train1, y_train1 = sampling_instance.fit_resample(X=X_train, y=y_train)
         else:
             X_train1, y_train1 = X_train, y_train
-        
+
         # Go through all models
         for model_instance in model_instances:
             print('fitting model ' + str(model_instances.index(model_instance) + 1) + ' over ' +  str(len(model_instances)))
@@ -48,7 +48,7 @@ def sampling(X_train, y_train, X_test, y_test, sampling_instances, model_instanc
     return metrics
 
 
-#e.g. : 
+#e.g. :
 """
 LR = LogisticRegression(random_state=0, solver='lbfgs', multi_class='ovr')
 SVM = svm.LinearSVC()
@@ -69,16 +69,16 @@ output = sampling(X_train, y_train, X_test, y_test undersamplings, models_instan
 
 def double_sampling(X_train, y_train, X_test, y_test, sampling_instances1, sampling_instances2, model_instances, func):
     """
-    Function to test different combination of sampling methods (over and under) on different models. 
-    For each combination of sampling methods and each model, it computes the set of metrics of metrics function 
+    Function to test different combination of sampling methods (over and under) on different models.
+    For each combination of sampling methods and each model, it computes the set of metrics of metrics function
 
     :input X_train, y_train, X_test, y_test: np.array or pd.DataFrame of data
     :input sampling_instance1: list of instances of sampling methods (tested for all methods of Imblearn) (1st round)
     :input sampling_instance2: list of instances of sampling methods (tested for all methods of Imblearn) (2nd round)
     :input model_instances: list of instances of models (from SKLearn or with SKLearn API)
     :input func: function to compute metrics - either compute_metrics or compute_main_metrics
-    
-    :output metrics: pd.Dataframe with metrics as column and model_x_sampling 
+
+    :output metrics: pd.Dataframe with metrics as column and model_x_sampling
     """
 
     list_of_df_metrics = []
@@ -86,7 +86,7 @@ def double_sampling(X_train, y_train, X_test, y_test, sampling_instances1, sampl
     for sampling_instance1 in  sampling_instances1:
         if sampling_instance1 is not None:
             print('fitting sampling1 '+ str(sampling_instances1.index(sampling_instance) + 1) + ' over ' +  str(len(sampling_instances1)))
-            
+
             X_train_1st_round, y_train1st_round = sampling_instance.fit_resample(X=X_train, y=y_train)
         else:
             X_train1st_round, y_train1st_round = X_train, y_train
@@ -100,11 +100,13 @@ def double_sampling(X_train, y_train, X_test, y_test, sampling_instances1, sampl
     return df_metrics_all
 
 
+def adjusted_classes(y_scores, t):
+    """
+    This function adjusts class predictions based on the prediction threshold (t)
 
+    :input y_scores: pd.DataFrame or np.array of predicted probability
+    :input t: probability thresold
 
-
-
-
-
-
-
+    :output: np.array of predicted label
+    """
+    return [1 if y >= t else 0 for y in y_scores]
