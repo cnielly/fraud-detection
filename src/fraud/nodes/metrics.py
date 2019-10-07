9a1b085745a41246233b529ee77b5a9a115518bc
mdeborne <mathieu.deborne@mail.mcgill.ca>
2019-10-07 14:45:19 +0200

    update metrics bis

--- src/fraud/nodes/metrics.py
+++ src/fraud/nodes/metrics.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import sklearn as sk
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, average_precision_score,confusion_matrix
 from imblearn.metrics import sensitivity_score, specificity_score, geometric_mean_score
 
